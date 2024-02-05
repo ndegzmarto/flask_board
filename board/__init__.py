5d3d9b7b4399d0d27a42b7b8dc9bf993d8102cc7
ndegz <ndegzmarto@gmail.com>
2024-02-05 22:34:22 +0300

    message board init app

---
+++ board/__init__.py
@@ -0,0 +1,12 @@
+"""application factory"""
+
+from flask import Flask 
+from board import pages
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.register_blueprint(pages.bp)
+
+    return app
+
